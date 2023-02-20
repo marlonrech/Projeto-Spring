94d0b9f6cca93674a34fa4c3d335858f592d4603
Marlon Rech <marlonrenan.rech@yahoo.com.br>
2023-02-20 19:20:55 -0300

    Adiciona Entities presentes no banco de dados

---
+++ src/main/java/tech/devinhouse/Projeto2Mod2/Model/Usuario.java
@@ -0,0 +1,24 @@
+package tech.devinhouse.Projeto2Mod2.Model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="usuario")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String senha;
+}
