0709262fe552e6a97b079561b2b2ad9099f87c05
Marlon Rech <marlonrenan.rech@yahoo.com.br>
2023-02-20 19:15:04 -0300

    Inicia repositório do projeto

---
+++ src/main/java/tech/devinhouse/Projeto2Mod2/Projeto2Mod2Application.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.Projeto2Mod2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projeto2Mod2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projeto2Mod2Application.class, args);
+	}
+
+}
---
+++ src/test/java/tech/devinhouse/Projeto2Mod2/Projeto2Mod2ApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.devinhouse.Projeto2Mod2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projeto2Mod2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
